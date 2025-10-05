c90f1279bae93f609ee4c2ccab05158ae86f57bb
KishiEdward <dzidanrh@gmail.com>
2025-10-05 13:18:08 +0700

    commit ketiga

--- lat_week3/bin/lat1.dart
+++ lat_week3/bin/lat1.dart
@@ -1,5 +1,9 @@
 void main(){ 
   var nama = "Kontol";
   nama = "Anjing";
+  final waktu = DateTime.now();
+  const phi = 3.14;
   print(nama); //akan mencetak Anjing
+  print(waktu); //akan mencetak waktu sekarang
+  print(phi); //akan mencetak 3.14
 }
