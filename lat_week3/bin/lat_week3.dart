3d896a07a07cbbcaf6366995e4a2874ab78a236d
KishiEdward <dzidanrh@gmail.com>
2025-10-05 13:01:26 +0700

    commit pertama

---
+++ lat_week3/bin/lat_week3.dart
@@ -0,0 +1,5 @@
+import 'package:lat_week3/lat_week3.dart' as lat_week3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lat_week3.calculate()}!');
+}
---
+++ lat_week3/test/lat_week3_test.dart
@@ -0,0 +1,8 @@
+import 'package:lat_week3/lat_week3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
